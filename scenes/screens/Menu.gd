a73cbcd9ac8891f4ee1379ded5160f8e12781d73
Richard <sanonnimmo@gmail.com>
2023-05-03 23:34:50 -0400

    tileset

--- scenes/screens/Menu.gd
+++ scenes/screens/Menu.gd
@@ -2,22 +2,26 @@ extends Node2D
 
 
 var bullets = []
-var shoots = 5
-var time_shoot = 2 * 60
-var last_shoot = 0
 
 func _ready():
 	pass
 		
 func _physics_process(delta):
-
 	if Input.is_action_just_pressed("attack"):
 		shoot()
+	if $Player.position.length() > 400:
+		$Player.position = -$Player.position
 		
 func shoot():
-	if shoots > 0:
-		var bullet = preload("res://scenes/attacks/Bullet_player.tscn").instance()
-		bullet.shoot($Player.position, $Player.rotation)
-		bullets.append(bullet)
-		add_child(bullet)
-		shoots-=1
+	var bullet = preload("res://scenes/attacks/Bullet_player.tscn").instance()
+	bullet.shoot($Player.position, $Player.rotation)
+	bullets.append(bullet)
+	add_child(bullet)
+
+
+func _on_Salir_pressed():
+	get_tree().quit()
+
+
+func _on_Jugar_pressed():
+	get_node("/root/Main").change_scene("res://scenes/screens/Game.tscn")
